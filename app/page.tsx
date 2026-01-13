031ee79658444a59a7c017d38080d060214fb024
Carson Strout <carson.strout42@gmail.com>
2026-01-12 21:45:14 -0700

    Initial website attempt

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,120 @@
+import Header from "@/components/layout/header";
+import Hero from "@/components/layout/hero";
+
 export default function Home() {
-  return <div>Place Holder</div>;
+	return (
+		<div>
+			<Header />
+			<Hero />
+
+			<p>
+				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi
+				molestias sed minima reprehenderit itaque suscipit debitis quis
+				doloribus nulla eligendi id, expedita omnis voluptatem, atque error
+				optio natus assumenda neque!
+			</p>
+			<p>
+				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi
+				molestias sed minima reprehenderit itaque suscipit debitis quis
+				doloribus nulla eligendi id, expedita omnis voluptatem, atque error
+				optio natus assumenda neque!
+			</p>
+			<p>
+				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi
+				molestias sed minima reprehenderit itaque suscipit debitis quis
+				doloribus nulla eligendi id, expedita omnis voluptatem, atque error
+				optio natus assumenda neque!
+			</p>
+			<p>
+				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi
+				molestias sed minima reprehenderit itaque suscipit debitis quis
+				doloribus nulla eligendi id, expedita omnis voluptatem, atque error
+				optio natus assumenda neque!
+			</p>
+			<p>
+				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi
+				molestias sed minima reprehenderit itaque suscipit debitis quis
+				doloribus nulla eligendi id, expedita omnis voluptatem, atque error
+				optio natus assumenda neque!
+			</p>
+			<p>
+				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi
+				molestias sed minima reprehenderit itaque suscipit debitis quis
+				doloribus nulla eligendi id, expedita omnis voluptatem, atque error
+				optio natus assumenda neque!
+			</p>
+			<p>
+				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi
+				molestias sed minima reprehenderit itaque suscipit debitis quis
+				doloribus nulla eligendi id, expedita omnis voluptatem, atque error
+				optio natus assumenda neque!
+			</p>
+			<p>
+				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi
+				molestias sed minima reprehenderit itaque suscipit debitis quis
+				doloribus nulla eligendi id, expedita omnis voluptatem, atque error
+				optio natus assumenda neque!
+			</p>
+			<p>
+				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi
+				molestias sed minima reprehenderit itaque suscipit debitis quis
+				doloribus nulla eligendi id, expedita omnis voluptatem, atque error
+				optio natus assumenda neque!
+			</p>
+			<p>
+				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi
+				molestias sed minima reprehenderit itaque suscipit debitis quis
+				doloribus nulla eligendi id, expedita omnis voluptatem, atque error
+				optio natus assumenda neque!
+			</p>
+			<p>
+				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi
+				molestias sed minima reprehenderit itaque suscipit debitis quis
+				doloribus nulla eligendi id, expedita omnis voluptatem, atque error
+				optio natus assumenda neque!
+			</p>
+			<p>
+				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi
+				molestias sed minima reprehenderit itaque suscipit debitis quis
+				doloribus nulla eligendi id, expedita omnis voluptatem, atque error
+				optio natus assumenda neque!
+			</p>
+			<p>
+				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi
+				molestias sed minima reprehenderit itaque suscipit debitis quis
+				doloribus nulla eligendi id, expedita omnis voluptatem, atque error
+				optio natus assumenda neque!
+			</p>
+			<p>
+				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi
+				molestias sed minima reprehenderit itaque suscipit debitis quis
+				doloribus nulla eligendi id, expedita omnis voluptatem, atque error
+				optio natus assumenda neque!
+			</p>
+			<p>
+				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi
+				molestias sed minima reprehenderit itaque suscipit debitis quis
+				doloribus nulla eligendi id, expedita omnis voluptatem, atque error
+				optio natus assumenda neque!
+			</p>
+			<p>
+				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi
+				molestias sed minima reprehenderit itaque suscipit debitis quis
+				doloribus nulla eligendi id, expedita omnis voluptatem, atque error
+				optio natus assumenda neque!
+			</p>
+			<p>
+				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi
+				molestias sed minima reprehenderit itaque suscipit debitis quis
+				doloribus nulla eligendi id, expedita omnis voluptatem, atque error
+				optio natus assumenda neque!
+			</p>
+			<p>
+				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi
+				molestias sed minima reprehenderit itaque suscipit debitis quis
+				doloribus nulla eligendi id, expedita omnis voluptatem, atque error
+				optio natus assumenda neque!
+			</p>
+		</div>
+	);
 }
