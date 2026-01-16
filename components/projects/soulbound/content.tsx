b136969147404731ffaf8732dfb472bb30c9afa6
Jackson Looney <jackslooney@gmail.com>
2026-01-15 21:34:12 -0600

    fix: lorum ipsum removed

--- components/projects/soulbound/content.tsx
+++ components/projects/soulbound/content.tsx
@@ -1,101 +1,10 @@
 const Content = () => {
-	return (
-		<section
-			id="content"
-			className="p-12 bg-linear-to-b from-gray-950 via-black to-gray-900 bg-[radial-gradient(circle_at_25%_15%,rgba(75,150,150,0.2),transparent_40%),radial-gradient(circle_at_75%_60%,rgba(64,224,208,0.15),transparent_50%),radial-gradient(circle_at_40%_85%,rgba(72,150,140,0.18),transparent_45%),radial-gradient(circle_at_60%_35%,rgba(48,100,100,0.12),transparent_60%)]"
-		>
-			<p>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
-				cupiditate accusantium voluptates quo eaque voluptatum labore repellat,
-				beatae dolor rem doloribus. Dignissimos mollitia animi quo rerum
-				voluptas enim atque distinctio.
-			</p>
-			<p>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
-				cupiditate accusantium voluptates quo eaque voluptatum labore repellat,
-				beatae dolor rem doloribus. Dignissimos mollitia animi quo rerum
-				voluptas enim atque distinctio.
-			</p>
-			<p>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
-				cupiditate accusantium voluptates quo eaque voluptatum labore repellat,
-				beatae dolor rem doloribus. Dignissimos mollitia animi quo rerum
-				voluptas enim atque distinctio.
-			</p>
-			<p>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
-				cupiditate accusantium voluptates quo eaque voluptatum labore repellat,
-				beatae dolor rem doloribus. Dignissimos mollitia animi quo rerum
-				voluptas enim atque distinctio.
-			</p>
-			<p>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
-				cupiditate accusantium voluptates quo eaque voluptatum labore repellat,
-				beatae dolor rem doloribus. Dignissimos mollitia animi quo rerum
-				voluptas enim atque distinctio.
-			</p>
-			<p>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
-				cupiditate accusantium voluptates quo eaque voluptatum labore repellat,
-				beatae dolor rem doloribus. Dignissimos mollitia animi quo rerum
-				voluptas enim atque distinctio.
-			</p>
-			<p>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
-				cupiditate accusantium voluptates quo eaque voluptatum labore repellat,
-				beatae dolor rem doloribus. Dignissimos mollitia animi quo rerum
-				voluptas enim atque distinctio.
-			</p>
-			<p>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
-				cupiditate accusantium voluptates quo eaque voluptatum labore repellat,
-				beatae dolor rem doloribus. Dignissimos mollitia animi quo rerum
-				voluptas enim atque distinctio.
-			</p>
-			<p>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
-				cupiditate accusantium voluptates quo eaque voluptatum labore repellat,
-				beatae dolor rem doloribus. Dignissimos mollitia animi quo rerum
-				voluptas enim atque distinctio.
-			</p>
-			<p>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
-				cupiditate accusantium voluptates quo eaque voluptatum labore repellat,
-				beatae dolor rem doloribus. Dignissimos mollitia animi quo rerum
-				voluptas enim atque distinctio.
-			</p>
-			<p>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
-				cupiditate accusantium voluptates quo eaque voluptatum labore repellat,
-				beatae dolor rem doloribus. Dignissimos mollitia animi quo rerum
-				voluptas enim atque distinctio.
-			</p>
-			<p>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
-				cupiditate accusantium voluptates quo eaque voluptatum labore repellat,
-				beatae dolor rem doloribus. Dignissimos mollitia animi quo rerum
-				voluptas enim atque distinctio.
-			</p>
-			<p>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
-				cupiditate accusantium voluptates quo eaque voluptatum labore repellat,
-				beatae dolor rem doloribus. Dignissimos mollitia animi quo rerum
-				voluptas enim atque distinctio.
-			</p>
-			<p>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
-				cupiditate accusantium voluptates quo eaque voluptatum labore repellat,
-				beatae dolor rem doloribus. Dignissimos mollitia animi quo rerum
-				voluptas enim atque distinctio.
-			</p>
-			<p>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
-				cupiditate accusantium voluptates quo eaque voluptatum labore repellat,
-				beatae dolor rem doloribus. Dignissimos mollitia animi quo rerum
-				voluptas enim atque distinctio.
-			</p>
-		</section>
-	);
+  return (
+    <section
+      id="content"
+      className="p-12 bg-linear-to-b from-gray-950 via-black to-gray-900 bg-[radial-gradient(circle_at_25%_15%,rgba(75,150,150,0.2),transparent_40%),radial-gradient(circle_at_75%_60%,rgba(64,224,208,0.15),transparent_50%),radial-gradient(circle_at_40%_85%,rgba(72,150,140,0.18),transparent_45%),radial-gradient(circle_at_60%_35%,rgba(48,100,100,0.12),transparent_60%)]"
+    ></section>
+  );
 };
 
 export default Content;
